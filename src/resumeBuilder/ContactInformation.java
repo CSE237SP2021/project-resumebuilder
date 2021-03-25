7dad52ad6688d9490b64c74f1ed2eaf5a8fa2606
Meenu Bhooshanan <meenubee@gmail.com>
2021-03-24 22:46:22 -0500

    First draft for ContactInformation and School classes

---
+++ src/resumeBuilder/ContactInformation.java
@@ -0,0 +1,31 @@
+package resumeBuilder;
+
+public class ContactInformation {
+	private String name;
+	private String email;
+	private String phone_number;
+	private String address;
+	
+	public ContactInformation (String name, String email, String phone_number, String address) {
+		this.name = name;
+		this.email = email;
+		this.phone_number = phone_number;
+		this.address = address;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public String getPhoneNumber() {
+		return this.phone_number;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+}
