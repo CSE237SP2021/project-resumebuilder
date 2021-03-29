d5c62c77ea2626f0518cd4ed8364d3537f3f2793
hannahanderson22 <54592476+hannahanderson22@users.noreply.github.com>
2021-03-28 19:52:05 -0500

    made progress on template menu and skill stories
    - program successfully prompts user for template type and accepts their
    selection
    - program does not prompt user for skills correctly
    - i outlined standardresume process procedure and some todos (future
    story ideas and discussion points)

--- src/resumeBuilder/Menu.java
+++ src/resumeBuilder/Menu.java
@@ -14,55 +14,102 @@ public class Menu {
 		resumeTemplateMenu.runTemplateMenu();
 	}
 	
-	public void runTemplateMenu() {
-		displayTemplateMenu();
-		
-		int selectedTemplate = getMenuSelection();
-		
-		openNewMenu(selectedTemplate);
-		
-		keyboardIn.close();
-		
-	}
-	
-	public void processTemplateMenu(int selectedTemplate) {
-		if(selectedTemplate == 1) {
-			
-			//Display standard menu
-			
-		} else if(selectedTemplate == 2) {
-			
-			//Display CV menu
-			
-		} else if(selectedTemplate == 3) {
-			
-			//Display technical menu
-			
-		} else {
-			return;
-		}
-		
-	}
-	
 	private int getMenuSelection() {
 		return keyboardIn.nextInt();
 	}
 	
 	
-	public void openNewMenu(int selectedTemplate) {
-		if(selectedTemplate == 1) {
-			
-		} else if(selectedTemplate == 2) {
-			
-		} else if(selectedTemplate == 3) {
-			
-		} else {
-			return;
-		}
+	private void runTemplateMenu() {
+		displayTemplateMenu();
+		
+		int selectedTemplate = getMenuSelection();
+		
+		processTemplateMenu(selectedTemplate);
+		
+		keyboardIn.close();
 		
 	}
 	
-	public void displayTemplateMenu() {
+	private void processTemplateMenu(int selectedTemplate) { //see displayTemplateMenu for code correspondence
+		if(selectedTemplate == 1) {
+			displayStandardResumeMenu();
+			
+			int standardResumeOption = getMenuSelection();
+			
+			processStandardResumeMenu(standardResumeOption);
+			
+		} else if(selectedTemplate == 2) {
+			
+			displayCVMenu();
+			
+			int CVOption = getMenuSelection();
+			
+			processCVMenu(CVOption);
+			
+		} else if(selectedTemplate == 3) {
+			
+			displayTechnicalResumeMenu();
+			
+			int technicalResumeOption = getMenuSelection();
+			
+			processTechnicalResumeMenu(technicalResumeOption);
+			
+		} else {
+			displayExitMessage();
+			return;
+		}
+	}
+	
+	private void processStandardResumeMenu(int standardResumeOption) {
+		/*
+		 * Hannah: I feel as though this is when we should instantiate our resume object that we keep track of?
+		 * The issue is that our constructor isn't empty; it requires contact info.
+		 * So maybe we should prompt for contact info immediately before showing the template menu? 
+		 * And then just pass that contactinfo object to processTemplateMenu, 
+		 * instantiate the resume with that contactinfo, and then pass the resume to this process method for further additions?
+		 */
+		if(standardResumeOption==1) {
+			//Prompt for contact information (menu?)
+			
+		} else if (standardResumeOption==2) {
+			
+			//Prompt for academic history (menu?)
+			
+		} else if (standardResumeOption==3) {
+			
+			//Prompt for work experience (menu?)
+			
+		} else if (standardResumeOption==4) {
+			
+			//Prompt for skills - definitely not a menu
+			
+			//Can't implement til we discuss resume instantiation, but my thought is something like
+			
+			System.out.println("Please enter the skill you would like to add.");
+			String newSkill = keyboardIn.nextLine(); // TODO For some reason this line is not accepting input as expected
+			
+			// currentStandardResume.addSkill(newSkill);
+			// displayStandardResumeMenu();
+			//int standardResumeOption = getMenuSelection();
+			//processStandardResumeMenu(standardResumeOption);
+			
+			// TODO make a method for each template that displays its menu, prompts for selection, and processes the selection so that we aren't repeating the same 3 lines of code
+		} else {
+			displayExitMessage();
+			// TODO update this else if standard resume menu changes (i expect it to)
+			return;
+		}
+	}
+	
+	private void processCVMenu(int CVOption) {
+		// TODO implement processCVMenu
+	}
+	
+	private void processTechnicalResumeMenu(int technicalResumeOption) {
+		// TODO implement processTechnicalResumeMenu
+	}
+	
+	private void displayTemplateMenu() {
 		System.out.println("What resume template would you like to use?");
 		System.out.println("1. Standard Resume");
 		System.out.println("2. CV");
@@ -70,7 +117,7 @@ public class Menu {
 		System.out.println("4. Exit");
 	}
 	
-	public void displayStandardResumeMenu() {
+	private void displayStandardResumeMenu() {
 		System.out.println("Please select the information you would like to add next.");
 		System.out.println("1. Contact Information");
 		System.out.println("2. Academic History");
@@ -80,7 +127,7 @@ public class Menu {
 		//Potentially change exit to "cancel" in the future and then have a "next" option that takes you to a formatting menu
 	}
 	
-	public void displayCVMenu() {
+	private void displayCVMenu() {
 		System.out.println("Please select the information you would like to add next.");
 		System.out.println("1. Contact Information");
 		System.out.println("2. Academic History");
@@ -95,7 +142,7 @@ public class Menu {
 		//Options based off of https://www.indeed.com/career-advice/resumes-cover-letters/cv-format-guide
 	}
 	
-	public void displayTechnicalResumeMenu() {
+	private void displayTechnicalResumeMenu() {
 		System.out.println("Please select the information you would like to add next.");
 		System.out.println("1. Contact Information");
 		System.out.println("2. Academic History");
@@ -106,6 +153,10 @@ public class Menu {
 		System.out.println("7. Programming Languages");
 		System.out.println("8. Exit");
 		//Potentially change exit to "cancel" in the future and then have a "next" option that takes you to a formatting menu
-
+	}
+	
+	private void displayExitMessage() {
+		System.out.println("Exiting Resume Builder...");
+		System.out.println("Goodbye!");
 	}
 }
