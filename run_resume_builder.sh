f1799d930b825ff33b38d6deff9555027490af00
Meenu <meenubee@gmail.com>
2021-03-29 11:09:17 -0500

    Added java compilation and run commands for the test folder

--- run_resume_builder.sh
+++ run_resume_builder.sh
@@ -2,4 +2,12 @@
 
 cd src/resumeBuilder
 javac *.java #Compiling all java files in a folder
-java Main #Running the program
+java Menu #Running the program
+
+cd ..
+cd tests
+javac *.java
+java ContactInformationTests
+java SchoolTests
+java ActivityTest
+java JobTest
